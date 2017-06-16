a9b6c51c92e7215a2b4d92a21324febe62aac8ed
Michele <mikele.p96@gmail.com>
2017-06-16 10:46:54 +0200

    Delete quintaScelta.sh

--- quintaScelta.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-: '
-in questa funzione stampo tutti i valori presenti nel file .vbox all interno del file databse, li esporto
-'
-
-echo -e "Esportazione di tutti i MAC ADDRESS contenuti nel file .vbox all'interno del database."
-grep -o -i 'macaddress=".*"' $1 | cut -c13-24 | sort -u >> $2
-sort -u $2 
